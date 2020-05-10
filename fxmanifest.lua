d641e67021d856f79b843dfe7785f39a36904916
abdulkadiraktas <abdulkadir.aktas@hotmail.com>
2020-05-10 11:45:17 +0300

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,12 +2,13 @@ fx_version 'adamant'
 
 clr_disable_task_scheduler 'yes'
 
+name 'CefLauncherWhitelist'
 game 'gta5'
 
 author 'Abdulkadir AKTAŞ'
 
 description 'Launcher Whitelist For Cef Launcher https://abdulkadiraktas.com.tr/sistemler/launchersystem/girisyap'
-repository 'https://github.com/tabarra/txAdmin'
+repository 'https://github.com/abdulkadiraktas/WhiteListForCefLauncher'
 
 version '1.1.0'
 
